cec1c7fec0d1228f39afc37f5b8038b32519c273
syy11cn <syy11cn@outlook.com>
2022-03-10 09:57:50 +0800

    chore: add husky hook

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,5 +2,20 @@ module.exports = {
   lang: 'zh-CN',
   title: 'Exp Talks',
   description: 'Aiming to help 1000 undergraduates.',
-  head: []
+  head: [],
+  plugins: [
+    [
+      '@vuepress/docsearch',
+      {
+        apiKey: '07d6e853853ae446b7f44f27cde218c8',
+        appId: 'M4N7ATG220',
+        placeholder: '搜索文档',
+        translations: {
+          button: {
+            buttonText: '搜索',
+          },
+        },
+      },
+    ],
+  ],
 };
