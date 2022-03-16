1ee40e523143c0738f6cab846df87a7cb3ae82e2
syy11cn <syy11cn@outlook.com>
2022-03-16 10:00:23 +0800

    chore: update navbar config

--- docs/.vuepress/config/navbar.ts
+++ docs/.vuepress/config/navbar.ts
@@ -1,14 +1,9 @@
 import { NavbarConfig } from "../types"
 
 export const navbar: NavbarConfig[] = [
-  {
-    text: '前言', "link": '/introduction/'
-  },
-  { 
-    text: '生存技巧', "link": '/survive/'
-  },
-  {
-    text: '找条出路', "link": '/run/'
-  },
+  { text: '前言', link: '/introduction/' },
+  { text: '择校', link: '/choose/' },
+  { text: '生存技巧', link: '/survive/' },
+  { text: '找条出路', link: '/run/' },
 ]
 
